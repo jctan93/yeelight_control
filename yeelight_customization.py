5de59ea08153ef91516efd0d9fbc49556883539f
Jian Chern, Tan <jctan93@gmail.com>
2021-09-26 23:45:26 +0800

    Added get_bulbs function to the yeelight_control class
    Signed-off-by: Jian Chern, Tan <jctan93@gmail.com>

--- yeelight_customization.py
+++ yeelight_customization.py
@@ -2,13 +2,28 @@
 
 from yeelight import Bulb
 from yeelight import discover_bulbs
+from pprint import pprint
 import yaml
 
 class yeelight_control:
     def __init__(self):
-        self.geek = "GeekforGeeks"
+        self.bulbs = discover_bulbs()
 
-    def user_prompt(device_list):
+    def get_bulbs(self):
+        devices = discover_bulbs()
+        self.devices_list = {}
+        counter = 1
+        for item in devices:
+            to_add = {}
+            to_add["ip"] = item["ip"]
+            to_add["model"] = item['capabilities']['model']
+            to_add["number"] = counter
+            self.devices_list[counter] = to_add
+            counter+=1
+
+        pprint(self.devices_list)
+
+    def user_prompt(self, device_list):
         print("Available devices: ")
         print(list(device_list.keys()))
 
@@ -20,14 +35,16 @@ if __name__ == "__main__":
     # Read devices.yaml file to get list of devices and their addresses
     devices = {}
 
-    with open(r'devices.yaml') as file:
-        devices = yaml.load(file, Loader=yaml.FullLoader)
+    # with open(r'devices.yaml') as file:
+    #     devices = yaml.load(file, Loader=yaml.FullLoader)
 
-    # Create a dictionary with key value pairs as <bulb_name, bulb_object>
-    bulbs = {}
-    for device in device_list.keys():
-        temp_device = Bulb()
+    # # Create a dictionary with key value pairs as <bulb_name, bulb_object>
+    # bulbs = {}
+    # for device in device_list.keys():
+    #     temp_device = Bulb()
+    yl_obj = yeelight_control()
+    yl_obj.get_bulbs()
 
-    user_prompt(devices)
+    # user_prompt(devices)
 
 
